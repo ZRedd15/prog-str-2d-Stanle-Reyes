f0e17641280731cd79e87c40ee14cbe2e50dd14c
MiguelPapuBv <20253ds075@utez.edu.mx>
2026-01-12 09:00:20 -0600

    Proyecto 01: hola mundo

---
+++ proyecto-01-hola-mundo/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Stanle Reyes Casimiro 2-D");
+        System.out.println("12 de Enero del 2026");
+    }
+}
