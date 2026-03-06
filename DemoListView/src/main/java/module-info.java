61b9d86a722cbd9dde3a61c1ac6c0e6cf7efc058
LeonardoArroyo <tu-correo@ejemplo.com>
2026-03-06 07:58:46 -0600

    Avance 2

--- DemoListView/src/main/java/module-info.java
+++ DemoListView/src/main/java/module-info.java
@@ -4,5 +4,11 @@ module com.example.demolistview {
 
 
     opens com.example.demolistview to javafx.fxml;
+    opens com.example.demolistview.controllers to javafx.fxml;
+    opens com.example.demolistview.repositores to javafx.fxml;
+    opens com.example.demolistview.services to javafx.fxml;
     exports com.example.demolistview;
+    exports com.example.demolistview.controllers;
+    exports com.example.demolistview.services;
+    exports com.example.demolistview.repositores;
 }
